fa087d93f5fe73ce207b90dcce1b86cb82417029
Jesse Carson <jmcarson222@gmail.com>
2023-02-20 15:55:26 -0600

    jwt

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -21,7 +21,17 @@ const login = async (req, res) => {
 
 // DASHBOARD
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
   const luckyNumber = Math.floor(Math.random() * 100); // 0-99
+
   res.status(200).json({
     msg: `Welcome, new user`,
     secret: `Here is your authorized data, your luck number is ${luckyNumber}`,
