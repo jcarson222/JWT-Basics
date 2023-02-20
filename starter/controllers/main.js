4b1d41ab5a2ca0b227f4c36c92fc3300960baf53
Jesse Carson <jmcarson222@gmail.com>
2023-02-20 11:51:54 -0600

    jwt setup

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -1,6 +1,22 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
 // LOGIN
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup Route");
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please enter a username and password", 400);
+  }
+
+  // just for demo. normally provided by the DB
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 // DASHBOARD
